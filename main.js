f0445d8b941810174a1f9bd37b6c00184cf06e24
Vegard Veiset <veiset@gmail.com>
2016-08-31 16:39:23 +0200

    fix.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import Firebase from 'firebase';
 import pixel from 'node-pixel';
 
 const firebase = new Firebase("https://fiery-inferno-7517.firebaseio.com/gadgets/neopixel");
+const fblight = new Firebase("https://fiery-inferno-7517.firebaseio.com/gadgets/lamp");
 const board = new five.Board();
 
 board.on("ready", function() {
@@ -11,30 +12,50 @@ board.on("ready", function() {
 
     // setup the node-pixel strip.
     let strip = new pixel.Strip({
-        strips: [{pin: 6, length: 60}],
+        strips: [{pin: 6, length: 50}],
         board: this,
         controller: "FIRMATA"
     });
 
+    function isNumberMax(n, max) {
+        return (n && parseInt(n) === n && parseInt(n) <= max && parseInt(n) >= 0);
+    }
+
     strip.on("ready", function () {
         firebase.child('light').on('value', d => {
-            try {
-                const data = JSON.parse(d.val());
+                console.log(d.val());
+                var data = d.val();
                 // Due to power draw, dont allow more than 15 lights
-                if (data.length > 15) {
+                if (!data || data.length > 59) {
                     return;
                 }
 
-                strip.off();
+                console.log(data);
                 data.forEach(([id, r, g, b]) => {
-                    strip.pixel(id).color(`rgba(${r}, ${g}, ${b}, 0)`);
+                    if (isNumberMax(id, 59) && isNumberMax(r, 255) && isNumberMax(g, 255) && isNumberMax(b, 255)) {
+                        r = parseInt(r);
+                        g = parseInt(g);
+                        b = parseInt(b);
+                        id = parseInt(id);
+                        strip.pixel(id).color(`rgba(${r}, ${g}, ${b}, 0)`);
+                    } else {
+                        console.log('Incorrect input');
+                    }
                 });
                 strip.show();
-            } catch (ex) {
-
-            }
 
         });
     });
 
+    var relay = new five.Relay(10);
+    fblight.child('switch').on('value', d => { 
+        var data = d.val();
+        console.log(data);
+
+        if (!data) {
+            relay.on();
+        } else {
+            relay.off();
+        }
+    });
 });
